0e0667ea8083388643e84fe260296d4f1f67aeb6
Ruitjes <stanrutten242@hotmail.com>
2021-09-30 10:23:08 +0200

    changes

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 const si = require('search-index');
+const levelup = require('levelup')
+const memdown = require('memdown')
 const moviesJSON = require('./movies28000.json'); 
 
 var movies = JSON.parse(JSON.stringify(moviesJSON))
 console.log(movies)
 
 async function asyncCall() {
-    // initialize an index
-    const searchIndex = await si();
+    const searchIndex = await si({db: levelup(memdown())});
 
-    await searchIndex.FLUSH()
+    console.log("SEARCH LIBRARY", searchIndex)
 
     const t0 = window.performance.now();
     // add documents to the index
@@ -16,14 +17,18 @@ async function asyncCall() {
     const t1 = window.performance.now();
     console.log('INDEXING TIME', t1 - t0)
 
+    document.getElementById("index").innerHTML = "INDEXING TIME: " + (t1 - t0) + " Msec";
+
     // read documents from the index
     const t2 = window.performance.now();
-    const results = await searchIndex.QUERY('title:a')
+    const results = await searchIndex.QUERY('the')
     const t3 = window.performance.now();
-    console.log('SEARCHING TIME', t3 - t2)
-    
-    console.log('SEARCH RESULTS', results);
 
+    console.log('SEARCHING TIME', t3 - t2)
+    document.getElementById("search").innerHTML = "SEARCHING TIME: " + (t3 - t2) + " Msec";
+
+    console.log('SEARCH RESULTS', results);
+    document.getElementById("result").innerHTML = "RESULT IN CONSOLE";
   }
 
 asyncCall()
